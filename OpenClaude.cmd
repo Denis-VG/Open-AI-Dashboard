f0e239fd32c49395fe3ccea60893975c70a20b65
Denis-VG <den.vgdf@gmail.com>
2026-08-13 15:47:42 +0300

    the project was renamed Open AI Dashboard

--- OpenClaude.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-call "%~dp0Scripts\activate.bat"
-rem Set up a separate folder for compiled Python files to keep your source code clutter-free.
-set "PYTHONPYCACHEPREFIX=%~dp0.pycache"
-if not exist "%PYTHONPYCACHEPREFIX%" mkdir "%PYTHONPYCACHEPREFIX%"
-python.exe "%~dp0dashboard\server.py"
